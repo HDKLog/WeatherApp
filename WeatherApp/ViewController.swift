1217cb49224116122737c799dfe346c21c83eda1
Harry S <gsarkisiani@bog.ge>
2021-08-26 14:51:52 +0400

    initial commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Gari Sarkisiani on 26.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Gari Sarkisiani on 25.08.21.
+//
+
+import XCTest
+@testable import WeatherApp
+
+class WeatherAppTests: XCTestCase {
+
+}

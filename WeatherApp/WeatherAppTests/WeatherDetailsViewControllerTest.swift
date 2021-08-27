1fd06cd49f2285d061f7f4028dbe0b9ca2413f67
Harry S <gsarkisiani@bog.ge>
2021-08-27 13:15:57 +0400

    add WeatherDetailsViewController Assert (Red)

---
+++ WeatherApp/WeatherAppTests/WeatherDetailsViewControllerTest.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherDetailsViewControllerTest.swift
+//  WeatherAppTests
+//
+//  Created by Gari Sarkisiani on 27.08.21.
+//
+
+import XCTest
+
+class WeatherDetailsViewControllerTest: XCTestCase {
+    
+    func viewController_renderTitleForViewModel() {
+        
+        //Arrange
+        
+        //Act
+        
+        //Assert (RED)
+        XCTAssertEqual(viewController.titleLabel.text, "a title")
+    }
+
+}

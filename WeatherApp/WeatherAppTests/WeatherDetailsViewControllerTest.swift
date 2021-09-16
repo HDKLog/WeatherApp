908ba3145eb76909ff85f2b0a60d9fed2a3cf74a
Harry S <gsarkisiani@bog.ge>
2021-09-16 16:37:40 +0400

    add share button test

--- WeatherApp/WeatherAppTests/WeatherDetailsViewControllerTest.swift
+++ WeatherApp/WeatherAppTests/WeatherDetailsViewControllerTest.swift
@@ -159,4 +159,40 @@ class WeatherDetailsViewControllerTest: XCTestCase {
         let cell = sut.parametersView.dataSource?.collectionView(sut.parametersView, cellForItemAt: IndexPath(row: 0, section: 0)) as? WeatherPropertyCollectionViewCell
         XCTAssertEqual(cell?.iconView.image, nil)
     }
+    class DummyPresenter: WeatherDetailsPresentation {
+        var wetherShareTriggered = false
+        var viewDidLoaded = false
+        func viewDidLoad() {
+            viewDidLoaded = true
+        }
+        
+        func shareWether() {
+            wetherShareTriggered = true
+        }
+        
+        
+    }
+    
+    func test_viewController_triggerActionForShareButton() {
+        
+        let presenter = DummyPresenter()
+        let sut = WeatherDetailsViewController()
+        sut.presenter = presenter
+        
+        let dataRequest = WeatherDescriptionViewModel.DataRequest { handler in
+            handler(Data())
+        }
+        let propertiesModel: [WeatherPropertyCellViewModel] = [WeatherPropertyCellViewModel(icon: nil, description: "100Unit")]
+        let descriptionModel = WeatherDescriptionViewModel(weatherImage: dataRequest,
+                                                           locationDescription: "a Loction Description",
+                                                           weatherDescription: "a Weather Description")
+        let model = WeatherDetailsViewModel(title: "a title",
+                                            weatherDescription: descriptionModel,
+                                            wetherParameters: propertiesModel)
+        sut.loadViewIfNeeded()
+        sut.configure(with: model)
+        sut.shareView.button.sendActions(for: .touchUpInside)
+        
+        XCTAssertTrue(presenter.wetherShareTriggered)
+    }
 }
